2932d9d34985b2c5b3f9a52f8f8cf518979e2a5f
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2021-11-30 15:23:22 +0100

    ouais ya des truc et tout

--- LumberJackSimulator/Assets/Scripts/Test.cs
+++ LumberJackSimulator/Assets/Scripts/Test.cs
@@ -1,29 +1,32 @@
+using Parabox.CSG;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
 using UnityEngine;
-using UnityEngine.XR;
 
 
-public class Test : MonoBehaviour
+public class test : MonoBehaviour
 {
-    bool leftGrip = false;
-    bool rightGrip = false;
+    public GameObject test1;
+    public GameObject test2;
 
-    bool leftTrigger = false;
-    bool rightTrigger = false;
-
-    private void OnLeftGrip(InputValue value)
+    public void Start()
     {
-        if (value.Get<float>() > .9f && !leftGrip)
-        {
-            leftGrip = true;
-            Debug.Log("leftGrip True");
-        }
-        if (value.Get<float>() < .1f && leftGrip)
-        {
-            leftGrip = false;
-            Debug.Log("leftGrip False");
-        }
+        // Initialize two new meshes in the scene
+/*        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = Vector3.one * 1.3f;*/
+
+
+        //// Perform boolean operation
+        Model result = CSG.Subtract(test1, test2);
+
+        // // Create a gameObject to render the result
+        var composite = new GameObject();
+        composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
+        composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+
+
     }
+
+
 }
